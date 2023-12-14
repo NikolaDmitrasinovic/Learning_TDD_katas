52f3e19ba2216ab6e150070ffe15cc10fab135d1
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 07:48:13 +0100

    refactor: improve code structure in HumanReadableTime class

--- HumanReadableTime/HumanReadableTime.Library/HumanReadableTime.cs
+++ HumanReadableTime/HumanReadableTime.Library/HumanReadableTime.cs
@@ -2,15 +2,11 @@
 {
     public class HumanReadableTime
     {
-        public static string GetReadableTime(int seconds)
+        public static string GetReadableTime(int timeInSeconds)
         {
-            if (seconds > 59)
+            if (timeInSeconds > 0)
             {
-                return "00:" + ConvertToDoubleDigit(seconds / 60) + ":" + ConvertToDoubleDigit(seconds % 60);
-            }
-            if (seconds > 0)
-            {                
-                return "00:00:" + ConvertToDoubleDigit(seconds);
+                return "00:" + ConvertToDoubleDigit(timeInSeconds / 60) + ":" + ConvertToDoubleDigit(timeInSeconds % 60);
             }
 
             return "00:00:00";
