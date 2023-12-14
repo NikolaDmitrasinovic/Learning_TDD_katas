00357706806fcd73428ff291078b29d67b793ed5
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 09:20:52 +0100

    refactor: improve code structure in tests

--- Bowling/BowlingGameTest/BowlingGameTests.cs
+++ Bowling/BowlingGameTest/BowlingGameTests.cs
@@ -5,33 +5,37 @@ namespace BowlingGameTest
 {
     public class BowlingGameTests
     {
+        private Game game = new();
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
         [Fact]
         public void Shoud_SolveGutterGame()
         {
             // Arrange
-            var game = new Game();
+            var rolls = 20;
+            var pins = 0;
 
             // Act
-            for(int i = 0; i < 20; i++)
-            {
-                game.Roll(0);
-            }
+            RollMany(20, 0);
 
             // Assert
             game.Score().Should().Be(0);
-        }
+        }        
 
         [Fact]
         public void Shoud_ResolveScore_ForAllOnes()
         {
             // Arrange
-            var game = new Game();
 
             // Act
-            for (int i = 0; i < 20; i++)
-            {
-                game.Roll(1);
-            }
+            RollMany(20, 1);
 
             // Assert
             game.Score().Should().Be(20);
