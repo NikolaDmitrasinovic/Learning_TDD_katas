fecda2d2413b17401f42f497e168fe07afad460e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 09:06:55 +0100

    test: resolve gutter game

--- Bowling/BowlingGame/Game.cs
+++ Bowling/BowlingGame/Game.cs
@@ -1,7 +1,16 @@
-﻿namespace BowlingGame
+﻿
+namespace BowlingGame
 {
     public class Game
     {
+        public void Roll(int v)
+        {
+            throw new NotImplementedException();
+        }
 
+        public object Score()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Bowling/BowlingGameTest/BowlingGameTests.cs
+++ Bowling/BowlingGameTest/BowlingGameTests.cs
@@ -1,11 +1,24 @@
+using BowlingGame;
+using FluentAssertions;
+
 namespace BowlingGameTest
 {
     public class BowlingGameTests
     {
         [Fact]
-        public void Test1()
+        public void Shoud_SolveGutterGame()
         {
+            // Arrange
+            var game = new Game();
 
+            // Act
+            for(int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            // Assert
+            game.Score().Should().Be(0);
         }
     }
 }
