eca46ee189a4364c6ab504b08b21b90ceaf6b1ac
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 15:27:55 +0100

    test: return deuce for 40-40

--- Tennis/TennisGameTests/GameTests.cs
+++ Tennis/TennisGameTests/GameTests.cs
@@ -19,5 +19,26 @@ namespace TennisGameTests
             // Assert
             game.CalculateScore().Should().Be("15-love");
         }
+
+        [Fact]
+        public void Should_ShowScoreAsDeuce_ForPlayerTiedAt40()
+        {
+            // Arrange
+            var player1 = new Player();
+            var player2 = new Player();
+            var game = new Game(player1, player2);
+
+            // Act
+            player1.PlayerScores();
+            player1.PlayerScores();
+            player1.PlayerScores();
+
+            player2.PlayerScores();
+            player2.PlayerScores();
+            player2.PlayerScores();
+
+            // Assert
+            game.CalculateScore().Should().Be("deuce");
+        }
     }
 }
