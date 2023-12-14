baeaa9f5923d94283ac99831f658df02fb7ab51d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 15:17:29 +0100

    feat: implement CalculateScore method

--- Tennis/TennisGame/Game.cs
+++ Tennis/TennisGame/Game.cs
@@ -1,7 +1,20 @@
-﻿namespace TennisGame
+﻿
+namespace TennisGame
 {
     public class Game
     {
+        private Player player1;
+        private Player player2;
 
+        public Game(Player _player1, Player _player2)
+        {
+            player1 = _player1;
+            player2 = _player2;
+        }
+
+        public string CalculateScore()
+        {
+            return ScoreConverter.Convert(player1.Score) + "-" + ScoreConverter.Convert(player2.Score);
+        }
     }
 }
