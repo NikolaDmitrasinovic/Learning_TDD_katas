8ec81a3cb197c31a8a9ef7989562bd86df5889c3
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 07:35:38 +0100

    test: get readable time for 5 seconds

--- HumanReadableTime/HumanReadableTime.Tests/HumanReadableTimeTests.cs
+++ HumanReadableTime/HumanReadableTime.Tests/HumanReadableTimeTests.cs
@@ -16,10 +16,21 @@ namespace HumanReadableTime.Tests
             // Assert
             readableTime.Should().Be("00:00:00");
         }
+
+        [Fact]
+        public void Should_ReturnCorrectTime_For5Seconds()
+        {
+            // Arrange
+
+            // Act
+            var readableTime = GetReadableTime(5);
+
+            // Assert
+            readableTime.Should().Be("00:00:05");
+        }
     }
 }
 
-//Assert.AreEqual(TimeFormat.GetReadableTime(5),00:00:05);
 //Assert.AreEqual(TimeFormat.GetReadableTime(60),00:01:00);
 //Assert.AreEqual(TimeFormat.GetReadableTime(86399),23:59:59);
 //Assert.AreEqual(TimeFormat.GetReadableTime(359999),99:59:59);
