a0aba17729447c8fd95ac1b0e4560d24e1ba893c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 14:22:32 +0100

    test: calculate score

--- Tennis/TennisGameTests/GameTests.cs
+++ Tennis/TennisGameTests/GameTests.cs
@@ -1,11 +1,22 @@
+using FluentAssertions;
+using TennisGame;
+
 namespace TennisGameTests
 {
     public class GameTests
     {
         [Fact]
-        public void Test1()
+        public void Should_ScoreFirstPoint_ForPlayer1()
         {
+            // Arrange
+            var game = new Game();
+            var player1 = new Player();
 
+            // Act
+            player1.PlayerScores();
+
+            // Assert
+            game.CalculateScore().Should().Be("15-love");
         }
     }
 }
