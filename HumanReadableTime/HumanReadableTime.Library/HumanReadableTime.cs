8f5c800aed06af5936c744c645e3d2fbf183d4d3
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 07:37:53 +0100

    feat: improve GetReadableTime

--- HumanReadableTime/HumanReadableTime.Library/HumanReadableTime.cs
+++ HumanReadableTime/HumanReadableTime.Library/HumanReadableTime.cs
@@ -4,6 +4,15 @@
     {
         public static string GetReadableTime(int seconds)
         {
+            if (seconds > 0)
+            {
+                if (seconds < 10)
+                {
+                    return "00:00:0" + seconds;
+                }
+                return "00:00:" + seconds;
+            }
+
             return "00:00:00";
         }
     }
