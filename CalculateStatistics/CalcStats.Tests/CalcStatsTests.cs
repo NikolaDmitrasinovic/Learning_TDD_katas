d4143a3e2674f35274ce0108c85dfd0031627ad8
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 04:56:45 +0100

    test: check for maximum

--- CalculateStatistics/CalcStats.Tests/CalcStatsTests.cs
+++ CalculateStatistics/CalcStats.Tests/CalcStatsTests.cs
@@ -16,5 +16,18 @@ namespace CalcStats.Tests
             // Assert
             minimumValue.Should().Be(-4);
         }
+
+        [Fact]
+        public void Should_ReturnMaximum_ForIntsArray()
+        {
+            // Arrange
+            var sequence = new Library.CalcStats([1, 2, 6, -4, 14, -1]);
+
+            // Act
+            var minimumValue = sequence.Maximum();
+
+            // Assert
+            minimumValue.Should().Be(14);
+        }
     }
 }
