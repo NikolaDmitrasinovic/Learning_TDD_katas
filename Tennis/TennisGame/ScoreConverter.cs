e08b788e64ed619fbbe5662ac5ffbdd3ab754c21
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-14 15:13:00 +0100

    feat: improve ScoreConverter

--- Tennis/TennisGame/ScoreConverter.cs
+++ Tennis/TennisGame/ScoreConverter.cs
@@ -10,6 +10,8 @@
                     return "15";
                 case 2:
                     return "30";
+                case 3:
+                    return "40";
                 default:
                     return "love";
             }
