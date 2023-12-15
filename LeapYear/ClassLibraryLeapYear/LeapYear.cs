191b028318c9bcdd8626b07a85ab665bbb869c28
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-15 08:51:58 +0100

    refactor: improve code structure in LeapYear

--- LeapYear/ClassLibraryLeapYear/LeapYear.cs
+++ LeapYear/ClassLibraryLeapYear/LeapYear.cs
@@ -4,13 +4,7 @@
     {
         public static bool IsLeapYear(int year)
         {
-            if (year % 100 == 0 && year % 400 != 0)
-                return false;
-
-            if (year % 4 == 0)
-                return true;
-
-            return false;
+            return year % 4 != 0 || year % 100 == 0 && year % 400 != 0 ? false : true;
         }
     }
 }
