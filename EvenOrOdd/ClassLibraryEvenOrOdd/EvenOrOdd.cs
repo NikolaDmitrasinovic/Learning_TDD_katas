243d05996444c54e45b6e407a8313fcf50278640
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-15 22:54:24 +0100

    refactor: improve code structure in EvenOrOdd

--- EvenOrOdd/ClassLibraryEvenOrOdd/EvenOrOdd.cs
+++ EvenOrOdd/ClassLibraryEvenOrOdd/EvenOrOdd.cs
@@ -5,9 +5,6 @@ namespace ClassLibraryEvenOrOdd
     {
         public static string OddOrEven(int[] intsArray)
         {
-            if (intsArray.Length == 0)
-                return "even";
-
             return intsArray.Sum() % 2 == 0 ? "even" : "odd";
         }
     }
