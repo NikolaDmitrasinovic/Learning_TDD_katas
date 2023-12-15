5e24b8ccfbd4fdd26ec191f5183903da2a860853
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-15 08:23:30 +0100

    test: check for common leap year

--- LeapYear/TestLeapYear/LeapYearTests.cs
+++ LeapYear/TestLeapYear/LeapYearTests.cs
@@ -15,5 +15,16 @@ namespace TestLeapYear
             // Assert
             LeapYear.IsLeapYear(2001).Should().BeFalse();
         }
+
+        [Fact]
+        public void Should_ReturnTrue_ForCommonLeapYear()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            LeapYear.IsLeapYear(1996).Should().BeTrue();
+        }
     }
 }
