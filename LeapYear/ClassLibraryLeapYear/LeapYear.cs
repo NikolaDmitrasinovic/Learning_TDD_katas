81b2cef34b2e5668370968d334c707b601b270c6
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-15 08:49:27 +0100

    feat: improve IsLeapYear

--- LeapYear/ClassLibraryLeapYear/LeapYear.cs
+++ LeapYear/ClassLibraryLeapYear/LeapYear.cs
@@ -4,6 +4,9 @@
     {
         public static bool IsLeapYear(int year)
         {
+            if (year % 100 == 0 && year % 400 != 0)
+                return false;
+
             if (year % 4 == 0)
                 return true;
 
