51490c23ae1a6ff210b608458d605f4dd42a3399
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-15 22:28:12 +0100

    test: return odd for one element array

--- EvenOrOdd/TestEvenOrOdd/EvenOrOddTests.cs
+++ EvenOrOdd/TestEvenOrOdd/EvenOrOddTests.cs
@@ -12,5 +12,17 @@ namespace TestEvenOrOdd
             // Assert
             EvenOrOdd.OddOrEven([]).Should().Be("even");
         }
+
+        [Theory]
+        [InlineData(new int[] { 1 }, "odd")]
+        public void Should_ReturnEvenOrOdd_ForOneElementArray(int[] intsArray, string result)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            EvenOrOdd.OddOrEven(intsArray).Should().Be(result);
+        }
     }
 }
