0a28c933dfcc09a16a69219269a0a3ab969d4b8f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-17 18:15:58 +0100

    test: check for 2 iterations

--- FizzBuzz/TestFizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/TestFizzBuzz/FizzBuzzTests.cs
@@ -13,5 +13,17 @@ namespace TestFizzBuzz
             // Assert
             gameResult.Should().BeEquivalentTo(["1"]);
         }
+
+        [Fact]
+        public void Should_Return12_For2Iteration()
+        {
+            // Arrange
+
+            // Act
+            var gameResult = FizzBuzz.Play(1);
+
+            // Assert
+            gameResult.Should().BeEquivalentTo(["1", "2"]);
+        }
     }
 }
