bea228beb46f7458a28232b890926aba5ad968e1
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-18 23:05:23 +0100

    feat: create the game

--- FizzBuzz/PlayFizzBuzz/Program.cs
+++ FizzBuzz/PlayFizzBuzz/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("For how many numbers would you like to paly(game starts at 1): ");
+            if (int.TryParse(Console.ReadLine(), out int n))
+            {
+                string[] playGame = FizzBuzz.Play(int.Abs(n));
+
+                for (int i = 0; i < playGame.Length; i++)
+                {
+                    Console.WriteLine(playGame[i]);
+                }
+            }
+            else
+                Console.WriteLine("You just don't know how to listen!");
         }
     }
 }
