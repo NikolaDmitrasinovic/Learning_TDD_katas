d1635862f63012bf7ffda9c475fb05c866632d0a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-18 22:51:47 +0100

    refactor: improve code structure in FizzBuzz refactor: improve code structure in tests

--- FizzBuzz/PlayFizzBuzz/FizzBuzz.cs
+++ FizzBuzz/PlayFizzBuzz/FizzBuzz.cs
@@ -1,32 +1,31 @@
-﻿namespace PlayFizzBuzz
+﻿using System.Text;
+
+namespace PlayFizzBuzz
 {
     public class FizzBuzz
     {
         public static string[] Play(int iterations)
         {
+            int[] numbers = Enumerable.Range(1, iterations).ToArray();
             string[] fizzBuzz = new string[iterations];
-            int number = 1;
 
             for (int i = 0; i < iterations; i++)
             {
-                if (Is3Multiplier(number))
-                {
-                    fizzBuzz[i] = "Fizz";
-                }
-                if (number % 5 == 0)
-                {
-                    fizzBuzz[i] += "Buzz";
-                }
-                if (string.IsNullOrEmpty(fizzBuzz[i]))
-                {
+                StringBuilder resultBuilder = new();
 
-                    fizzBuzz[i] = number.ToString();
-                }
+                resultBuilder.Append(Is3Multiplier(numbers[i]) ? "Fizz" : "");
+                resultBuilder.Append(Is5Multiplier(numbers[i]) ? "Buzz" : "");
 
-                number++;
+                fizzBuzz[i] = resultBuilder.Length > 0 ? resultBuilder.ToString() : numbers[i].ToString();
+
             }
 
             return fizzBuzz;
+        }
+
+        private static bool Is5Multiplier(int number)
+        {
+            return number % 5 == 0;
         }
 
         private static bool Is3Multiplier(int number)
--- FizzBuzz/TestFizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/TestFizzBuzz/FizzBuzzTests.cs
@@ -2,52 +2,21 @@ namespace TestFizzBuzz
 {
     public class FizzBuzzTests
     {
-        [Fact]
-        public void Should_Return1_For1Iteration()
+        [Theory]
+        [InlineData(1, new string[] { "1" })]
+        [InlineData(2, new string[] { "1", "2" })]
+        [InlineData(3, new string[] { "1", "2", "Fizz" })]
+        [InlineData(5, new string[] { "1", "2", "Fizz", "4", "Buzz" })]
+        [InlineData(15, new string[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" })]
+        public void Should_Return1_For1Iteration(int iterations, string[] result)
         {
             // Arrange
 
             // Act
-            var gameResult = FizzBuzz.Play(1);
+            var gameResult = FizzBuzz.Play(iterations);
 
             // Assert
-            gameResult.Should().BeEquivalentTo(["1"]);
-        }
-
-        [Fact]
-        public void Should_Return12_For2Iteration()
-        {
-            // Arrange
-
-            // Act
-            var gameResult = FizzBuzz.Play(2);
-
-            // Assert
-            gameResult.Should().BeEquivalentTo(["1", "2"]);
-        }
-
-        [Fact]
-        public void Should_ReturnFizz_For3Multiplayer()
-        {
-            // Arrange
-
-            // Act
-            var gameResult = FizzBuzz.Play(3);
-
-            // Assert
-            gameResult.Should().BeEquivalentTo(["1", "2", "Fizz"]);
-        }
-
-        [Fact]
-        public void Should_ReturnBuzz_For5Multiplayer()
-        {
-            // Arrange
-
-            // Act
-            var gameResult = FizzBuzz.Play(5);
-
-            // Assert
-            gameResult.Should().BeEquivalentTo(["1", "2", "Fizz", "4", "Buzz"]);
+            gameResult.Should().BeEquivalentTo(result);
         }
     }
 }
