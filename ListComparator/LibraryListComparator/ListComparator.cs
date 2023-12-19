308802b7af63fbaf4cd05e11588fba1152f1f7dd
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-19 22:31:56 +0100

    refactor: improve code structure in Compare()

--- ListComparator/LibraryListComparator/ListComparator.cs
+++ ListComparator/LibraryListComparator/ListComparator.cs
@@ -2,27 +2,9 @@
 {
     public class ListComparator
     {
-        public static int Compare(int[] intsArray1, int[] intsArray2)
+        public static int Compare(int[] subject, int[] reference)
         {
-            if (intsArray1 != intsArray2)
-            {
-                int numberOfSameElements = 0;
-
-                for (int i = 0; i < intsArray1.Length; i++)
-                {
-                    for (int n = 0; n < intsArray2.Length; n++)
-                    {
-                        if (intsArray1[i] == intsArray2[n])
-                        {
-                            numberOfSameElements++;
-                        }
-                    }
-                }
-
-                return numberOfSameElements;
-            }
-
-            return intsArray1.Length;
+            return subject.Intersect(reference).Count();
         }
     }
 }
