e5102c0ccbdd845f0c5310907450ab2cc406e4c8
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-19 22:01:55 +0100

    feat: implement Compare()

---
+++ ListComparator/LibraryListComparator/ListComparator.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryListComparator
+{
+    public class ListComparator
+    {
+        public static int Compare(int[] intsArray1, int[] intsArray2)
+        {
+            return 1;
+        }
+    }
+}
