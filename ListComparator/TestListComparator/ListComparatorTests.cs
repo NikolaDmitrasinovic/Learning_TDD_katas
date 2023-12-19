4cf77780348442a63b01ab687b9b185ec4a6d14e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-19 22:39:20 +0100

    refactor: improve code structure in tests

--- ListComparator/TestListComparator/ListComparatorTests.cs
+++ ListComparator/TestListComparator/ListComparatorTests.cs
@@ -2,30 +2,18 @@ namespace TestListComparator
 {
     public class ListComparatorTests
     {
-        [Fact]
-        public void Should_Return1_For1ElementArrayComparedtoItself()
+        [Theory]
+        [InlineData(new int[] { 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, 2)]
+        public void Should_ReturnNumberOfSameElements_ForArrayComparedtoItself(int[] intsArray, int result)
         {
             // Arrange
-            var intsArray = new[] { 1 };
 
             // Act
             var numberOfSameElements = ListComparator.Compare(intsArray, intsArray);
 
             // Assert
-            numberOfSameElements.Should().Be(1);
-        }
-
-        [Fact]
-        public void Should_Return2_For2ElementsArrayComparedtoItself()
-        {
-            // Arrange
-            var intsArray = new[] { 1, 2 };
-
-            // Act
-            var numberOfSameElements = ListComparator.Compare(intsArray, intsArray);
-
-            // Assert
-            numberOfSameElements.Should().Be(2);
+            numberOfSameElements.Should().Be(result);
         }
 
         [Fact]
