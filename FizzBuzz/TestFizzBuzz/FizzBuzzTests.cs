bde57edeb2c4fa34c67b994a4a0d1f1fd080efe2
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-19 16:47:51 +0100

    test: add test for result occurance

--- FizzBuzz/TestFizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/TestFizzBuzz/FizzBuzzTests.cs
@@ -8,7 +8,7 @@ namespace TestFizzBuzz
         [InlineData(3, new string[] { "1", "2", "Fizz" })]
         [InlineData(5, new string[] { "1", "2", "Fizz", "4", "Buzz" })]
         [InlineData(15, new string[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz" })]
-        public void Should_Return1_For1Iteration(int iterations, string[] result)
+        public void Should_PlayFizzBuzz_ForSetNumberOfIterations(int iterations, string[] result)
         {
             // Arrange
 
@@ -17,6 +17,22 @@ namespace TestFizzBuzz
 
             // Assert
             gameResult.Should().BeEquivalentTo(result);
+        }
+
+        [Theory]
+        [InlineData(100, 14, 89, "FizzBuzz")]
+        [InlineData(100, 2, 98, "Fizz")]
+        [InlineData(100, 4, 99, "Buzz")]
+        public void Should_ReturnFizzBuzz_ForSetNumberOfIterations(int iterations, int firstOccurence, int lastOccurence, string result)
+        {
+            // Arrange
+
+            // Act
+            var gameResult = FizzBuzz.Play(iterations);
+
+            // Assert
+            gameResult.Should().HaveElementAt(firstOccurence, result)
+                .And.HaveElementAt(lastOccurence, result);
         }
     }
 }
