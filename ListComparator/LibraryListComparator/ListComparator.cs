3073dd2422e8bdb4eedd5aadc33c59226d1bcb64
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-19 22:24:15 +0100

    feat: improve Compare()

--- ListComparator/LibraryListComparator/ListComparator.cs
+++ ListComparator/LibraryListComparator/ListComparator.cs
@@ -4,6 +4,24 @@
     {
         public static int Compare(int[] intsArray1, int[] intsArray2)
         {
+            if (intsArray1 != intsArray2)
+            {
+                int numberOfSameElements = 0;
+
+                for (int i = 0; i < intsArray1.Length; i++)
+                {
+                    for (int n = 0; n < intsArray2.Length; n++)
+                    {
+                        if (intsArray1[i] == intsArray2[n])
+                        {
+                            numberOfSameElements++;
+                        }
+                    }
+                }
+
+                return numberOfSameElements;
+            }
+
             return intsArray1.Length;
         }
     }
