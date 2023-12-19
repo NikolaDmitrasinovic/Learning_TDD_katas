a2d9b03828d3b31544a4eee5ce50ef9bf6704841
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-19 22:12:22 +0100

    feat: improve Compae()

--- ListComparator/LibraryListComparator/ListComparator.cs
+++ ListComparator/LibraryListComparator/ListComparator.cs
@@ -4,6 +4,11 @@
     {
         public static int Compare(int[] intsArray1, int[] intsArray2)
         {
+            if (intsArray1.Length > 1)
+            {
+                return intsArray1.Length;
+            }
+
             return 1;
         }
     }
