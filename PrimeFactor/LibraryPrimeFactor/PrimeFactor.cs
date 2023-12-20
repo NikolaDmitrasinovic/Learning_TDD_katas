fd4d72c5e9d7f04447596bd0fe461caff6948917
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-20 22:39:36 +0100

    feat: improve Generate()

--- PrimeFactor/LibraryPrimeFactor/PrimeFactor.cs
+++ PrimeFactor/LibraryPrimeFactor/PrimeFactor.cs
@@ -5,11 +5,15 @@
         public static List<int> Generate(int number)
         {
             List<int> primeFactors = [];
+            int prime = 2;
 
-            while(number >= 2)
+            while (number >= 2)
             {
-                number /= 2;
-                primeFactors.Add(2);
+                while (number % prime != 0)
+                    prime++;
+
+                number /= prime;
+                primeFactors.Add(prime);
             }
 
             return primeFactors;
