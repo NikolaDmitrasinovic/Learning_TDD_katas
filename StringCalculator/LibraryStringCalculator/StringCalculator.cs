5b9fba7898a69cd11e23dfce2272c9166e6c9fb5
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-21 19:38:15 +0100

    feat: improve Add()

--- StringCalculator/LibraryStringCalculator/StringCalculator.cs
+++ StringCalculator/LibraryStringCalculator/StringCalculator.cs
@@ -7,6 +7,9 @@
             if (string.IsNullOrEmpty(numbersString))
                 return 0;
 
+            if (numbersString.Length == 3)
+                return int.Parse(numbersString[0].ToString()) + int.Parse(numbersString[2].ToString());
+
             return int.Parse(numbersString);
         }
     }
