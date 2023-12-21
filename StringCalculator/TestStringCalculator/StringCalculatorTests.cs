626ad9d3e8b7fecef1dfbd9779b7f2a55aa98e6c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-21 19:31:33 +0100

    test: return int from string input

--- StringCalculator/TestStringCalculator/StringCalculatorTests.cs
+++ StringCalculator/TestStringCalculator/StringCalculatorTests.cs
@@ -13,5 +13,17 @@ namespace TestStringCalculator
             // Assert
             result.Should().Be(0);
         }
+
+        [Fact]
+        public void Should_ReturnInt_ForStringInput()
+        {
+            // Arrange
+
+            // Act
+            var result = StringCalculator.Add("1");
+
+            // Assert
+            result.Should().Be(1);
+        }
     }
 }
