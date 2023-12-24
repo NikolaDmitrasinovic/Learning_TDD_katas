bfc165f83929dab23a12b2c29f8ad49d5183483e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-24 19:18:24 +0100

    test: handle multiple delimiters of any length

--- StringCalculator/TestStringCalculator/StringCalculatorTests.cs
+++ StringCalculator/TestStringCalculator/StringCalculatorTests.cs
@@ -105,5 +105,17 @@ namespace TestStringCalculator
             // Assert
             result.Should().Be(3);
         }
+
+        [Fact]
+        public void Should_HandleMultipleDelimitersOfAnyLength()
+        {
+            // Arrange
+
+            // Act
+            var result = StringCalculator.Add("//[**][&&&&]\n1*1&1");
+
+            // Assert
+            result.Should().Be(3);
+        }
     }
 }
