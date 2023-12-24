2176911f17ae862052c92d3848efd8a84edbbf7e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-24 19:01:10 +0100

    refactore: remove private method

--- StringCalculator/LibraryStringCalculator/StringCalculator.cs
+++ StringCalculator/LibraryStringCalculator/StringCalculator.cs
@@ -8,16 +8,9 @@
 
             IEnumerable<int> numbers = numbersString.Split(delimiter).Select(s => int.TryParse(s, out int n) ? n : 0);
 
-            numbers = RemoveLargeNumbers(numbers);
+            CheckForNegativeNumbers(numbers);
 
-            CheckForNegativeNumbers(numbers);            
-
-            return numbers.Sum();
-        }
-
-        private static IEnumerable<int> RemoveLargeNumbers(IEnumerable<int> numbers)
-        {
-            return numbers.Where(x => x <= 1000);
+            return numbers.Sum(num => num > 1000 ? 0 : num);
         }
 
         private static void CheckForNegativeNumbers(IEnumerable<int> numbers)
