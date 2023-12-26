d5dd42d508d6507eda9a0abdabee2f1116771c8e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-26 16:43:22 +0100

    refactor: improve code structure in WordWrapper

--- WordWrapper/LibraryWordWrapper/WordWrapper.cs
+++ WordWrapper/LibraryWordWrapper/WordWrapper.cs
@@ -4,26 +4,12 @@ namespace LibraryWordWrapper
 {
     public class WordWrapper
     {
-        public static string Wrap(string textToWrap, int columnNumber = 0)
+        public static string Wrap(string textToWrap, int columnNumber = int.MaxValue)
         {
-            if (columnNumber == 0)
+            if (textToWrap.Length <= columnNumber)
                 return textToWrap;
 
-            if (textToWrap.Length == 2)
-                return textToWrap[0] + "\n" + textToWrap[1];
-
-            StringBuilder sb = new StringBuilder();
-            sb.Append(textToWrap[0]);
-            for (int i = 1; i < textToWrap.Length; i++)
-            {
-                if (i % columnNumber == 0)
-                {
-                    sb.Append("\n");
-                }
-                sb.Append(textToWrap[i]);
-            }
-
-            return sb.ToString();
+            return textToWrap[..columnNumber] + "\n" + Wrap(textToWrap[columnNumber..], columnNumber);
         }
     }
 }
