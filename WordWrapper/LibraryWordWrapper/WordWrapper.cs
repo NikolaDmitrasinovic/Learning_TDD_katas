82f38944efc6e097eff30cf92dc3ca5e8726c45c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-26 17:16:59 +0100

    feat: improve Wrap()

--- WordWrapper/LibraryWordWrapper/WordWrapper.cs
+++ WordWrapper/LibraryWordWrapper/WordWrapper.cs
@@ -9,6 +9,9 @@ namespace LibraryWordWrapper
             if (textToWrap.Length <= columnNumber || columnNumber == 0)
                 return textToWrap;
 
+            if (textToWrap[columnNumber - 2] == ' ')
+                return textToWrap.Substring(0, columnNumber - 1) + "\n" + Wrap(textToWrap.Substring(columnNumber - 1).TrimStart(), columnNumber);
+
             return textToWrap[..columnNumber] + "\n" + Wrap(textToWrap[columnNumber..].TrimStart(), columnNumber);
         }
     }
