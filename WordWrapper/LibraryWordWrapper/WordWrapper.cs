dc9a3c08e6ddc5b8e96f662dfd8e5e48d5769043
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-26 12:32:05 +0100

    feat: improve Wrap()

--- WordWrapper/LibraryWordWrapper/WordWrapper.cs
+++ WordWrapper/LibraryWordWrapper/WordWrapper.cs
@@ -1,4 +1,6 @@
-﻿namespace LibraryWordWrapper
+﻿using System.Text;
+
+namespace LibraryWordWrapper
 {
     public class WordWrapper
     {
@@ -7,7 +9,21 @@
             if (columnNumber == 0)
                 return textToWrap;
 
-            return textToWrap[0] + "\n" + textToWrap[1];
+            if (textToWrap.Length == 2)
+                return textToWrap[0] + "\n" + textToWrap[1];
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(textToWrap[0]);
+            for (int i = 1; i < textToWrap.Length; i++)
+            {
+                if (i % columnNumber == 0)
+                {
+                    sb.Append("\n");
+                }
+                sb.Append(textToWrap[i]);
+            }
+
+            return sb.ToString();
         }
     }
 }
