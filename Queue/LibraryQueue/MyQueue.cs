77e91e34692e1687d444abc5e10d1b8edd2608af
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-28 22:45:52 +0100

    feat: implement Enqueue()

--- Queue/LibraryQueue/MyQueue.cs
+++ Queue/LibraryQueue/MyQueue.cs
@@ -1,7 +1,22 @@
-﻿namespace LibraryQueue
+﻿
+namespace LibraryQueue
 {
-    public class MyQueue
+    public class MyQueue<T>
     {
+        private List<T> queue;
 
+        public int Count { get; private set; }
+
+        public MyQueue()
+        {
+            Count = 0;
+            queue = [];
+        }
+
+        public void Enqueue(T element)
+        {
+            queue.Add(element);
+            Count++;
+        }
     }
 }
