b84252de61b7d970c346a341278270605f956ba4
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-29 20:59:56 +0100

    test: should add multiple elements to queue

--- Queue/TestQueue/MyQueueTests.cs
+++ Queue/TestQueue/MyQueueTests.cs
@@ -14,5 +14,21 @@
             // Assert
             myQueue.Count.Should().Be(1);
         }
+
+        [Fact]
+        public void Should_AddSomeElementsToQueue()
+        {
+            // Arrange
+            var myQueue = new MyQueue<int>();
+
+            // Act
+            for (int i = 0; i < 10; i++)
+            {
+                myQueue.Enqueue(i);
+            }
+
+            // Assert
+            myQueue.Count.Should().Be(10);
+        }
     }
 }
