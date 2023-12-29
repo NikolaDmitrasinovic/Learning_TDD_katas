1bf576d3ecb201fcecbfcb5f003138e50923b552
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2023-12-29 21:06:18 +0100

    refactor: improve tests readability

--- Queue/TestQueue/MyQueueTests.cs
+++ Queue/TestQueue/MyQueueTests.cs
@@ -2,33 +2,22 @@
 {
     public class MyQueueTests
     {
-        [Fact]
-        public void Should_AddElementToQueue()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        public void Should_AddElementsToQueue(int numberOfElements)
         {
             // Arrange
             var myQueue = new MyQueue<int>();
 
             // Act
-            myQueue.Enqueue(1);
-
-            // Assert
-            myQueue.Count.Should().Be(1);
-        }
-
-        [Fact]
-        public void Should_AddSomeElementsToQueue()
-        {
-            // Arrange
-            var myQueue = new MyQueue<int>();
-
-            // Act
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < numberOfElements; i++)
             {
                 myQueue.Enqueue(i);
             }
 
             // Assert
-            myQueue.Count.Should().Be(10);
+            myQueue.Count.Should().Be(numberOfElements);
         }
     }
 }
