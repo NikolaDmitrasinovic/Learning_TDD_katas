5bb8d7db3b4c96fe7fb4b89903a5b8258a1fd5cd
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-02 17:48:16 +0100

    feat: implement Push()

--- Stack/LibraryStack/MyStack.cs
+++ Stack/LibraryStack/MyStack.cs
@@ -1,7 +1,22 @@
-﻿namespace LibraryStack
+﻿
+namespace LibraryStack
 {
     public class MyStack
     {
+        public int Count { get; set; }
 
+        private List<object> items;
+
+        public MyStack()
+        {
+            Count = 0;
+            items = new List<object>();
+        }
+
+        public void Push(object v)
+        {
+            items.Add(v);
+            Count++;
+        }
     }
 }
