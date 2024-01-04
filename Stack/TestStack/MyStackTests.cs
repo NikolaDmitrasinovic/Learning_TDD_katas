83a858327157e6b1c2c8079f0adbdeb1d864dc2f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-04 08:50:46 +0100

    test: should return last element

--- Stack/TestStack/MyStackTests.cs
+++ Stack/TestStack/MyStackTests.cs
@@ -14,5 +14,20 @@ namespace TestStack
             // Assert
             myStack.Count.Should().Be(1);
         }
+
+        [Fact]
+        public void Should_ReturnLastElement()
+        {
+            // Arrange
+            var myStack = new MyStack();
+
+            // Act
+            myStack.Push(5);
+            myStack.Push(6);
+
+            // Assert
+            myStack.Pop().Should().Be(2);
+            myStack.Count.Should().Be(1);
+        }
     }
 }
