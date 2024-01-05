dfa898101b6b262e26d7e10fa3f614debe73e525
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-05 09:32:18 +0100

    test: should add item to list

--- RecentlyUsedList/TestRecentlyUsedList/RecentlyUsedListTests.cs
+++ RecentlyUsedList/TestRecentlyUsedList/RecentlyUsedListTests.cs
@@ -13,5 +13,18 @@ namespace TestRecentlyUsedList
             // Assert
             recentlyUsedList.NumberOfRecentItems.Should().Be(0);
         }
+
+        [Fact]
+        public void Should_AddItem_ToTheList()
+        {
+            // Arrange
+            var recentlyUsedList = new RecentlyUsedList();
+
+            // Act
+            recentlyUsedList.AddRecentItem("item1");
+
+            // Assert
+            recentlyUsedList.NumberOfRecentItems.Should().Be(0);
+        }
     }
 }
