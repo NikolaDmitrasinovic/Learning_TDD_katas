9f2cc42f705af174acda49f0db9a0ed3fe1937f3
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-07 18:01:10 +0100

    test: shold return -1 for unfinished game

--- TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
+++ TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
@@ -3,9 +3,21 @@ namespace TestTicTacToe
     public class TicTacToeCheckerTests
     {
         [Fact]
-        public void Test1()
+        public void Should_ReturnNegativeOne_ForUnsolvedGame()
         {
+            // Arrange
+            var game = new int[3, 3]
+            {
+                { 0, 0, 0 },
+                { 0, 0, 0 },
+                { 0, 0, 0 }
+            };
 
+            // Act
+            var gameResult = TicTacToeChecker.Check(game);
+
+            // Assert
+            gameResult.Should().Be(-1);
         }
     }
 }
