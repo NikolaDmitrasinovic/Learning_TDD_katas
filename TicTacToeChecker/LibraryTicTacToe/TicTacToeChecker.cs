ccdfba5af58bc2c11a570c7cd056fa2d979905e0
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-07 18:02:07 +0100

    feat: implement Check()

--- TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
+++ TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
@@ -1,7 +1,11 @@
-﻿namespace LibraryTicTacToe
+﻿
+namespace LibraryTicTacToe
 {
     public class TicTacToeChecker
     {
-
+        public static int Check(int[,] game)
+        {
+            return -1;
+        }
     }
 }
