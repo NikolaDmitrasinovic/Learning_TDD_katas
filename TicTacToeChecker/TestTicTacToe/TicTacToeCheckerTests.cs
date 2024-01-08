204f3f48555fbda5ca2f9248fa20e0169288d0aa
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-08 09:33:17 +0100

    test: should return 1 for x victory

--- TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
+++ TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
@@ -19,5 +19,23 @@ namespace TestTicTacToe
             // Assert
             gameResult.Should().Be(-1);
         }
+
+        [Fact]
+        public void Should_ReturnOne_ForXVictory()
+        {
+            // Arrange
+            var game = new int[3, 3]
+            {
+                { 1, 1, 1 },
+                { 0, 2, 0 },
+                { 0, 0, 2 }
+            };
+
+            // Act
+            var gameResult = TicTacToeChecker.Check(game);
+
+            // Assert
+            gameResult.Should().Be(1);
+        }
     }
 }
