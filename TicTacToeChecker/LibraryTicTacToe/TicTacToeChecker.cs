71610808db2a7c61d60b42b0279e06721e99012c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-08 10:20:02 +0100

    feat: improve Check()

--- TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
+++ TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
@@ -9,6 +9,8 @@ namespace LibraryTicTacToe
                 return 1;
             if (game[1, 0] == 1 && game[1, 1] == 1 && game[1, 2] == 1)
                 return 1;
+            if (game[2, 0] == 1 && game[2, 1] == 1 && game[2, 2] == 1)
+                return 1;
 
             if (game[0, 0] == 2 && game[0, 1] == 2 && game[0, 2] == 2)
                 return 2;
