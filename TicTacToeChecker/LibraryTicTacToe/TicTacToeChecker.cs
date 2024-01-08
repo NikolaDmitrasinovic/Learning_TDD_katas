2805831ec6d67307a771624e728704c236b47f17
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-08 11:52:33 +0100

    feat: implement IsVictoryCrossColumn()

--- TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
+++ TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
@@ -5,10 +5,10 @@ namespace LibraryTicTacToe
     {
         public static int Check(int[,] game)
         {
-            if (IsVictroyRow(game, 1) || IsVictroyColumn(game, 1))
+            if (IsVictroyRow(game, 1) || IsVictroyColumn(game, 1) || IsVictroyCrossColumn(game, 1))
                 return 1;
 
-            if (IsVictroyRow(game, 2) || IsVictroyColumn(game, 2))
+            if (IsVictroyRow(game, 2) || IsVictroyColumn(game, 2) || IsVictroyCrossColumn(game, 2))
                 return 2;
 
             return -1;
@@ -28,6 +28,17 @@ namespace LibraryTicTacToe
             for (int i = 0; i < 3; i++)
                 if (game[0, i] == player && game[1, i] == player && game[2, i] == player)
                     return true;
+
+            return false;
+        }
+
+        private static bool IsVictroyCrossColumn(int[,] game, int player)
+        {
+            if (game[0, 0] == player && game[1, 1] == player && game[2, 2] == player)
+                return true;
+
+            if (game[0, 2] == player && game[1, 1] == player && game[2, 0] == player)
+                return true;
 
             return false;
         }
