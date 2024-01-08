f8ab878bf9550d5fc1c30f5dcf42d18884c4eedf
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-08 11:35:26 +0100

    test: should return victor for column

--- TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
+++ TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
@@ -23,14 +23,34 @@ namespace TestTicTacToe
         [Theory]
         [InlineData(1)]
         [InlineData(2)]
-        public void Should_ReturnVictor_ForFirstRowVictory(int victor)
+        public void Should_ReturnVictor_ForRow(int victor)
         {
             // Arrange
             var game = new int[3, 3]
             {
                 { victor, victor, victor},
-                { 0, 2, 0 },
-                { 0, 0, 2 }
+                { 0, 0, 0 },
+                { 0, 0, 0 }
+            };
+
+            // Act
+            var gameResult = TicTacToeChecker.Check(game);
+
+            // Assert
+            gameResult.Should().Be(victor);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void Should_ReturnVictor_ForColumn(int victor)
+        {
+            // Arrange
+            var game = new int[3, 3]
+            {
+                { 0, 0, victor},
+                { 0, 0, victor },
+                { 0, 0, victor }
             };
 
             // Act
