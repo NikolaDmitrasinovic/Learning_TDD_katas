b2ae9bf4a9b9133feceb1185688ab9fcf7659943
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-08 11:46:49 +0100

    feat: implement IsVictroyColumn()

--- TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
+++ TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
@@ -5,19 +5,31 @@ namespace LibraryTicTacToe
     {
         public static int Check(int[,] game)
         {
-            int playerX = CheckVictory(game, 1);
-            int playerO = CheckVictory(game, 2);
+            if (IsVictroyRow(game, 1) || IsVictroyColumn(game, 1))
+                return 1;
 
-            return playerX == 1 ? playerX : playerO;
+            if (IsVictroyRow(game, 2) || IsVictroyColumn(game, 2))
+                return 2;
+
+            return -1;
         }
 
-        private static int CheckVictory(int[,] game, int player)
+        private static bool IsVictroyRow(int[,] game, int player)
         {
             for (int i = 0; i < 3; i++)
                 if (game[i, 0] == player && game[i, 1] == player && game[i, 2] == player)
-                    return player;
+                    return true;
 
-            return -1;
+            return false;
+        }
+
+        private static bool IsVictroyColumn(int[,] game, int player)
+        {
+            for (int i = 0; i < 3; i++)
+                if (game[0, i] == player && game[1, i] == player && game[2, i] == player)
+                    return true;
+
+            return false;
         }
     }
 }
