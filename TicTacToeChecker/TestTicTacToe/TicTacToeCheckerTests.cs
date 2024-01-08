e90b9fe3420cbc0d22a0f54b9ac3b96e4c3dba53
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-08 12:09:33 +0100

    chore: change arrange

--- TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
+++ TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
@@ -8,9 +8,9 @@ namespace TestTicTacToe
             // Arrange
             var game = new int[3, 3]
             {
-                { 0, 0, 0 },
-                { 0, 0, 0 },
-                { 0, 0, 0 }
+                { 2, 2, 1 },
+                { 2, 1, 1 },
+                { 0, 1, 2 }
             };
 
             // Act
