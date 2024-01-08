741037257b89a8d913b6e6f26458f4b4c3ee2866
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-08 10:24:44 +0100

    refactor: improve code structure of TicTacToeChecker

--- TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
+++ TicTacToeChecker/LibraryTicTacToe/TicTacToeChecker.cs
@@ -5,15 +5,17 @@ namespace LibraryTicTacToe
     {
         public static int Check(int[,] game)
         {
-            if (game[0,0] == 1 && game[0,1] == 1 && game[0,2] == 1)
-                return 1;
-            if (game[1, 0] == 1 && game[1, 1] == 1 && game[1, 2] == 1)
-                return 1;
-            if (game[2, 0] == 1 && game[2, 1] == 1 && game[2, 2] == 1)
-                return 1;
+            for (int i = 0; i < 3; i++)
+            {
+                if (game[i, 0] == 1 && game[i, 1] == 1 && game[i, 2] == 1)
+                    return 1;
+            }
 
-            if (game[0, 0] == 2 && game[0, 1] == 2 && game[0, 2] == 2)
-                return 2;
+            for (int i = 0; i < 3; i++)
+            {
+                if (game[i, 0] == 2 && game[i, 1] == 2 && game[i, 2] == 2)
+                    return 2;
+            }
 
             return -1;
         }
