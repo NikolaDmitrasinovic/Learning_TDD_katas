f7eb22b66527806cedbe45020457748faa46d5a7
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-08 11:33:09 +0100

    refactor: remove tests

--- TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
+++ TicTacToeChecker/TestTicTacToe/TicTacToeCheckerTests.cs
@@ -39,45 +39,5 @@ namespace TestTicTacToe
             // Assert
             gameResult.Should().Be(victor);
         }
-
-        [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        public void Should_ReturnVictor_ForSecondRowVictory(int victor)
-        {
-            // Arrange
-            var game = new int[3, 3]
-            {
-                { 0, 0, 2 },
-                { victor, victor, victor},
-                { 0, 2, 0 }                
-            };
-
-            // Act
-            var gameResult = TicTacToeChecker.Check(game);
-
-            // Assert
-            gameResult.Should().Be(victor);
-        }
-
-        [Theory]
-        [InlineData(1)]
-        [InlineData(2)]
-        public void Should_ReturnVictor_ForThirdRowVictory(int victor)
-        {
-            // Arrange
-            var game = new int[3, 3]
-            {
-                { 0, 2, 0 },
-                { 0, 0, 2 },
-                { victor, victor, victor}                
-            };
-
-            // Act
-            var gameResult = TicTacToeChecker.Check(game);
-
-            // Assert
-            gameResult.Should().Be(victor);
-        }
     }
 }
