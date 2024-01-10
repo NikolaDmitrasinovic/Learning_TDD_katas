6a786bbb55622b1339ccdb0d62dfbff39b4dceae
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-10 14:22:49 +0100

    test: return true for valid solution

--- HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
+++ HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
@@ -3,8 +3,26 @@ namespace TestHashiwokakero
     public class HashiValidatorTests
     {
         [Fact]
-        public void Test1()
+        public void Validate_ReturnsTrue_ForValidSolution()
         {
+            // Arrange
+            var puzzle = new HashiPuzzle();
+
+            // Act
+            var isValid = HashiValidator.Validate(puzzle);
+
+            // Assert
+            isValid.Should().BeTrue();
+        }
+
+        [Fact(Skip = "future test")]
+        public void Validate_ReturnsFalse_ForInvalidSolution()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
 
         }
     }
