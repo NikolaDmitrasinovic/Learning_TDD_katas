31f5c1ef00c1b92d44b702db82f0667642688c72
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-11 11:18:36 +0100

    chore: change values

--- HashiSolver/TestHashiwokakero/HashiPuzzleTests.cs
+++ HashiSolver/TestHashiwokakero/HashiPuzzleTests.cs
@@ -13,14 +13,14 @@
             puzzle.AddIsle(1);
             puzzle.AddIsle(2);
             puzzle.AddIsle(2);
+            puzzle.AddBridge(0, 2);
             puzzle.AddBridge(1, 3);
-            puzzle.AddBridge(2, 4);
-            puzzle.AddBridge(3, 4);
-            puzzle.AddBridge(3, 4);
+            puzzle.AddBridge(2, 3);
+            puzzle.AddBridge(2, 3);
 
             // Assert
             puzzle.IsleList.Count.Should().Be(4);
-            puzzle.BrigdeList.Count.Should().Be(4);
+            puzzle.BridgeList.Count.Should().Be(4);
         }
     }
 }
