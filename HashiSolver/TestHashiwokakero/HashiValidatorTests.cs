4b1bba8cf1425e3814d0d819240f44119ceefa6f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-11 11:22:44 +0100

    test: return false for empty puzzle

--- HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
+++ HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
@@ -3,6 +3,19 @@ namespace TestHashiwokakero
     public class HashiValidatorTests
     {
         [Fact]
+        public void Validate_ReturnsFalse_ForEmptyPuzzle()
+        {
+            // Arrange
+            var puzzle = new HashiPuzzle();
+
+            // Act
+            var isValid = HashiValidator.Validate(puzzle);
+
+            // Assert
+            isValid.Should().BeFalse();
+        }
+
+        [Fact(Skip = "future test")]
         public void Validate_ReturnsTrue_ForValidSolution()
         {
             // Arrange
