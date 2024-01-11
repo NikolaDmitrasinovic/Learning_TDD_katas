63ffb2ae0154b7ba26aa8d685941b51cdf1b10eb
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-11 11:31:53 +0100

    test: validate that every island has needed amount of bridges

--- HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
+++ HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
@@ -15,11 +15,20 @@ namespace TestHashiwokakero
             isValid.Should().BeFalse();
         }
 
-        [Fact(Skip = "future test")]
-        public void Validate_ReturnsTrue_ForValidSolution()
+        [Fact]
+        public void Validate_ThatEveryIslandHasNeededAmountOfBridges()
         {
             // Arrange
             var puzzle = new HashiPuzzle();
+
+            puzzle.AddIsle(1);
+            puzzle.AddIsle(1);
+            puzzle.AddIsle(2);
+            puzzle.AddIsle(2);
+            puzzle.AddBridge(0, 2);
+            puzzle.AddBridge(1, 3);
+            puzzle.AddBridge(2, 3);
+            puzzle.AddBridge(2, 3);
 
             // Act
             var isValid = HashiValidator.Validate(puzzle);
