3134985bd50a268a40f3bce6a928e3c228812af0
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-11 11:19:00 +0100

    feat: implement new class HashiPuzzle

--- HashiSolver/ClassLibraryHashiwokakero/HashiPuzzle.cs
+++ HashiSolver/ClassLibraryHashiwokakero/HashiPuzzle.cs
@@ -1,9 +1,19 @@
-﻿namespace ClassLibraryHashiwokakero
+﻿
+namespace ClassLibraryHashiwokakero
 {
     public class HashiPuzzle
     {
-        public HashiPuzzle()
+        public List<HashiIsle> IsleList { get; set; } = new List<HashiIsle>();
+        public List<HashiBridge> BridgeList { get; set; } = new List<HashiBridge>();
+
+        public void AddBridge(int v1, int v2)
         {
+            BridgeList.Add(new HashiBridge(IsleList[v1], IsleList[v2]));
+        }
+
+        public void AddIsle(int value)
+        {
+            IsleList.Add(new HashiIsle(value));
         }
     }
 }
