8a02ea827382dd9c7cfe88732a8f9d14676a5e0f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-11 09:28:19 +0100

    feat: implement HashiIsle

---
+++ HashiSolver/ClassLibraryHashiwokakero/HashiIsle.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ClassLibraryHashiwokakero
+{
+    public class HashiIsle(int value)
+    {
+        public int Value { get; } = value;
+        public List<HashiIsle> ConnectedIsles { get; private set; } = [];
+
+        public HashiBridge Connect(HashiIsle isle)
+        {
+            ConnectedIsles.Add(isle);
+            return new HashiBridge(this, isle);
+        }
+    }
+}
