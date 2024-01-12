d7b8d602bebd0e230072a830a0fb68c784dc1cc0
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-12 11:06:57 +0100

    test: return false if bridges are crossing

--- HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
+++ HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
@@ -37,24 +37,23 @@ namespace TestHashiwokakero
         }
 
         [Fact]
-        public void Validate_NoBridgeIsCrossingWithAnother()
+        public void Validate_ReturnFalse_IfBridgesAreCrossing()
         {
             // Arrange
             var puzzle = new HashiPuzzle();
 
-            puzzle.AddIsle(1, 0, 0);
             puzzle.AddIsle(1, 0, 1);
-            puzzle.AddIsle(2, 1, 0);
-            puzzle.AddIsle(2, 1, 1);
+            puzzle.AddIsle(1, 1, 0);
+            puzzle.AddIsle(1, 2, 1);
+            puzzle.AddIsle(1, 1, 2);
             puzzle.AddBridge(0, 2);
             puzzle.AddBridge(1, 3);
-            puzzle.AddBridge(2, 3);
 
             // Act
             var isValid = HashiValidator.Validate(puzzle);
 
             // Assert
-            isValid.Should().BeTrue();
+            isValid.Should().BeFalse();
         }
 
         [Fact(Skip = "future test")]
