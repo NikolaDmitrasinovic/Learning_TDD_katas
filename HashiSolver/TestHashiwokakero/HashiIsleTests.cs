1dc4c903c5a163664ae2a810d83379bcc0675f0e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-12 09:47:36 +0100

    test: instantiate isle correctly

--- HashiSolver/TestHashiwokakero/HashiIsleTests.cs
+++ HashiSolver/TestHashiwokakero/HashiIsleTests.cs
@@ -3,11 +3,26 @@
     public class HashiIsleTests
     {
         [Fact]
+        public static void Instantiate_IsleCorrectly()
+        {
+            // Arrange
+            var isle = new HashiIsle(2, 1, 1);
+
+            // Act
+
+            // Assert
+            isle.Value.Should().Be(2);
+            isle.ConnectedIsles.Count.Should().Be(0);
+            isle.X.Should().Be(1);
+            isle.Y.Should().Be(1);
+        }
+
+        [Fact]
         public static void ConnectIsle_AddsIsleToConnectedIsleList()
         {
             // Arrange
-            var isle1 = new HashiIsle(2);
-            var isle2 = new HashiIsle(1);
+            var isle1 = new HashiIsle(2, 1, 1);
+            var isle2 = new HashiIsle(1, 1, 1);
 
             // Act
             var bridge = isle1.Connect(isle2);
