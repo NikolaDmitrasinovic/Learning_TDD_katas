697f0792ff8748dceadc97880ccf5b923537dd08
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-12 13:29:11 +0100

    feat: improve Validate() refactor: extract methods

--- HashiSolver/ClassLibraryHashiwokakero/HashiValidator.cs
+++ HashiSolver/ClassLibraryHashiwokakero/HashiValidator.cs
@@ -1,32 +1,73 @@
-﻿namespace ClassLibraryHashiwokakero
+﻿
+namespace ClassLibraryHashiwokakero
 {
     public class HashiValidator
     {
         public static bool Validate(HashiPuzzle puzzle)
         {
-            if (IsNotReady(puzzle))
+            if (!IsReady(puzzle))
                 return false;
-            else if (IsNotBridgeCovered(puzzle))
+            else if (!IsBridgeCovered(puzzle))
+                return false;
+            else if (!IsCorrectlyBridged(puzzle))
                 return false;
 
             return true;
         }
 
-        private static bool IsNotReady(HashiPuzzle puzzle)
+        private static bool IsCorrectlyBridged(HashiPuzzle puzzle)
         {
-            if (puzzle.Isles.Count == 0)
-                return true;
+            foreach (var bridge1 in puzzle.Bridges)
+            {
+                foreach (var bridge2 in puzzle.Bridges)
+                {
+                    if (!bridge1.Equals(bridge2))
+                        continue;
+
+                    if (AreCrossing(bridge1, bridge2))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool AreCrossing(HashiBridge bridge1, HashiBridge bridge2)
+        {
+            if (bridge1.Isle1.X == bridge1.Isle2.X && bridge2.Isle1.Y == bridge2.Isle2.Y)
+            {
+                if (bridge1.Isle1.Y < bridge2.Isle1.Y && bridge1.Isle2.Y > bridge2.Isle1.Y)
+                {
+                    return true;
+                }
+            }
+
+            if (bridge1.Isle1.Y == bridge1.Isle2.Y && bridge2.Isle1.X == bridge2.Isle2.X)
+            {
+                if (bridge1.Isle1.X < bridge2.Isle1.X && bridge1.Isle2.X > bridge2.Isle1.X)
+                {
+                    return true;
+                }
+            }
 
             return false;
         }
 
-        private static bool IsNotBridgeCovered(HashiPuzzle puzzle)
+        private static bool IsReady(HashiPuzzle puzzle)
+        {
+            if (puzzle.Isles.Count == 0)
+                return false;
+
+            return true;
+        }
+
+        private static bool IsBridgeCovered(HashiPuzzle puzzle)
         {
             foreach (var isle in puzzle.Isles)
                 if (isle.ConnectedIsles.Count != isle.Value)
-                    return true;
+                    return false;
 
-            return false;
+            return true;
         }
     }
 }
