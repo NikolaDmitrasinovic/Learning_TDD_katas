28625c7ad0690f404c46194919f8f49e9d52e40e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-12 09:47:57 +0100

    feat: add props to HashiIsle

--- HashiSolver/ClassLibraryHashiwokakero/HashiIsle.cs
+++ HashiSolver/ClassLibraryHashiwokakero/HashiIsle.cs
@@ -1,10 +1,12 @@
 ﻿
 namespace ClassLibraryHashiwokakero
 {
-    public class HashiIsle(int value)
+    public class HashiIsle(int value, int x, int y)
     {
         public int Value { get; } = value;
         public List<HashiIsle> ConnectedIsles { get; private set; } = [];
+        public object X { get; } = x;
+        public object Y { get; } = y;
 
         public HashiBridge Connect(HashiIsle isle)
         {
