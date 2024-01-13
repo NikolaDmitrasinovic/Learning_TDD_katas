47d5453ff87391192d12ddfbf8abeb25b890c47a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-13 17:52:27 +0100

    test: return false if bridges are not orthagonal

--- HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
+++ HashiSolver/TestHashiwokakero/HashiValidatorTests.cs
@@ -55,5 +55,22 @@ namespace TestHashiwokakero
             // Assert
             isValid.Should().BeFalse();
         }
+
+        [Fact]
+        public void Validate_ReturnFalse_IfBridgesAreNotOrthagonal()
+        {
+            // Arrange
+            var puzzle = new HashiPuzzle();
+
+            puzzle.AddIsle(1, 0, 1);
+            puzzle.AddIsle(1, 1, 0);
+            puzzle.AddBridge(0, 1);
+
+            // Act
+            var isValid = HashiValidator.Validate(puzzle);
+
+            // Assert
+            isValid.Should().BeFalse();
+        }
     }
 }
