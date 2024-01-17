b518b8dadadc97d4016cfe61a14a433f9239fd4d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-17 12:06:34 +0100

    refactor: extract methods

--- HashiSolver/ClassLibraryHashiwokakero/HashiBridge.cs
+++ HashiSolver/ClassLibraryHashiwokakero/HashiBridge.cs
@@ -8,13 +8,13 @@
 
         public HashiBridge(HashiIsle isle1, HashiIsle isle2)
         {
-            if (isle1.X == isle2.X)
+            if (IsHorizontal(isle1, isle2))
             {
                 Orientation = BridgeOrientation.Horizontal;
                 IsleFrom = isle1.Y < isle2.Y ? isle1 : isle2;
                 IsleTo = isle1.Y > isle2.Y ? isle1 : isle2;
             }
-            else if (isle1.Y == isle2.Y)
+            else if (IsVertical(isle1, isle2))
             {
                 Orientation = BridgeOrientation.Vertical;
                 IsleFrom = isle1.X < isle2.X ? isle1 : isle2;
@@ -22,6 +22,16 @@
             }
             else
                 throw new InvalidOperationException("Only orthagonal bridges allowed.");
+        }
+
+        private static bool IsVertical(HashiIsle isle1, HashiIsle isle2)
+        {
+            return isle1.Y == isle2.Y;
+        }
+
+        private static bool IsHorizontal(HashiIsle isle1, HashiIsle isle2)
+        {
+            return isle1.X == isle2.X;
         }
     }
 
