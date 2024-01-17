8ce3f76963d631e379b0ff9fa799e65ab9527766
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-17 10:52:37 +0100

    fix: change isle position refactor: change prop names

--- HashiSolver/TestHashiwokakero/HashiBridgeTests.cs
+++ HashiSolver/TestHashiwokakero/HashiBridgeTests.cs
@@ -7,14 +7,14 @@
         {
             // Arrange
             var isle1 = new HashiIsle(1, 1, 1);
-            var isle2 = new HashiIsle(2, 1, 1);
+            var isle2 = new HashiIsle(2, 1, 2);
 
             // Act
             var bridge = new HashiBridge(isle1, isle2);
 
             // Assert
-            bridge.Isle1.Should().Be(isle1);
-            bridge.Isle2.Should().Be(isle2);
+            bridge.IsleFrom.Should().Be(isle1);
+            bridge.IsleTo.Should().Be(isle2);
         }
 
         [Fact]
