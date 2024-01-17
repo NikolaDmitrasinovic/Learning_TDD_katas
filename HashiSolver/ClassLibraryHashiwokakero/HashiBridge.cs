1557cc9f3557592b58ab3f55030874a1ae3100de
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-01-17 10:53:51 +0100

    feat: add new prop and change ctor

--- HashiSolver/ClassLibraryHashiwokakero/HashiBridge.cs
+++ HashiSolver/ClassLibraryHashiwokakero/HashiBridge.cs
@@ -1,8 +1,31 @@
 ﻿namespace ClassLibraryHashiwokakero
 {
-    public class HashiBridge(HashiIsle isleFrom, HashiIsle isleTo)
+    public class HashiBridge
     {
-        public HashiIsle Isle1 { get; } = isleFrom;
-        public HashiIsle Isle2 { get; } = isleTo;
+        public HashiIsle IsleFrom { get; }
+        public HashiIsle IsleTo { get; }
+        public BridgeOrientation Orientation { get; }
+
+        public HashiBridge(HashiIsle isle1, HashiIsle isle2)
+        {
+            if (isle1.X == isle2.X)
+            {
+                Orientation = BridgeOrientation.Horizontal;
+                IsleFrom = isle1.Y < isle2.Y ? isle1 : isle2;
+                IsleTo = isle1.Y > isle2.Y ? isle1 : isle2;
+            }
+            else if (isle1.Y == isle2.Y)
+            {                
+                Orientation = BridgeOrientation.Vertical;
+                IsleFrom = isle1.X < isle2.X ? isle1 : isle2;
+                IsleTo = isle1.X > isle2.X ? isle1 : isle2;
+            }        
+        }
+    }
+
+    public enum BridgeOrientation
+    {
+        Horizontal,
+        Vertical
     }
 }
